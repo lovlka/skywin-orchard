f81a6b3ac945d04365da537d0b6925ded23584e7
Victor Stodell <victor@stodell.se>
2015-03-11 18:38:53 +0100

    Added stubs for SkyWin orchard module

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+using Orchard.Themes;
+
+namespace SkyWin.Controllers {
+    [Themed]
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View("SkyWin");
+        }
+    }
+}
---
+++ Routes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace SkyWin {
+    public class Routes : IRouteProvider {
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "SkyWin",
+                        new RouteValueDictionary {
+                            {"area", "SkyWin"},
+                            {"controller", "Home"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "SkyWin"}
+                        },
+                        new MvcRouteHandler())
+                }
+            };
+        }
+    }
+}
